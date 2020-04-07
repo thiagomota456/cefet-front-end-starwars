1334f2f2da253415f258dffc243faf94ad870022
Thiago Soares Mota <thiagomota456@gmail.com>
2020-04-07 01:53:07 -0300

    0 e 1 prontos. Apanhando do 2

--- js/starwars.js
+++ js/starwars.js
@@ -3,3 +3,22 @@
 // para fazer uma requisição assíncrona e:
 //  - Pegar a lista de filmes (AJAX) e preencher no HTML
 //  - Quando um filme for clicado, exibir sua introdução
+$.ajax({
+    url: 'https://swapi.co/api/films/',
+    dataType: 'json',
+    success: function(resposta) {
+      console.log(resposta);
+
+      let titulos = resposta.results
+      let index = 4
+      var movies  = ''
+      titulos.forEach(element => {
+          movies = '<li>'+ element.title + '</li>'
+          //$('<div>'+ element.title + '</div>').appendTo(document.body);
+          $('ul li').html(element)
+      });
+      //$local_de_filme = $("#filmes ul")
+
+      
+    }
+  });
